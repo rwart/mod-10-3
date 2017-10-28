58e1b4e1af51e2879d287b4b3a3c28279721b54a
rwart <32432397+rwart@users.noreply.github.com>
2017-10-28 15:24:58 +0200

    ready for review

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,9 @@ $(function () {
   'use strict';
   var $caroselList = $('#carosel ul');
   setInterval(changeSlide, 3000);
-  console.log('after setInterval()');
 
   function changeSlide() {
     $caroselList.animate({ marginLeft: -314 }, 500, moveFirstSlide);
-    console.log('changeSlide');
   }
 
   function moveFirstSlide() {
@@ -14,7 +12,6 @@ $(function () {
     var lastItem  = $caroselList.find('li:last');
     lastItem.after(firstItem);
     $caroselList.css({ marginLeft: 0 });
-    console.log('moveFirstSlide');
   }
 
 });
